0bb1dcc1cf9ad2c7f4164eea3fe9c0cf9e72e7ee
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-08-09 18:54:08 -0300

    Stored data in file

---
+++ src/data/index.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: 'Concentre-se apenas no trabalho importante',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis. Donec non ullamcorper sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+  },
+  {
+    title: 'Diga adeus ao trabalho maçante',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis. Donec non ullamcorper sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+  },
+  {
+    title: 'Armazenamento e compartilhamento de arquivos são só o começo',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis.',
+  },
+  {
+    title: 'Para todos',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    title: 'Parte inferior da página',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis.',
+  },
+];
