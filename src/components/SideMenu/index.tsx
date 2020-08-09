dd6e73dec1fc12ff32ca8707e0b3df347eac784c
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-08-09 20:59:15 -0300

    Debugging scroll

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Container } from './styles';
 
 const SideMenu: React.FC = ({ children }) => {
+  useEffect(() => {
+    function onScroll() {
+      console.log(window.scrollY);
+    }
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return <Container>{children}</Container>;
 };
 
